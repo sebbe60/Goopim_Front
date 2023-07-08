be6fde1761189ab1f9180b0c2940b16b77ac4bd0
sebbe60 <130213748+sebbe60@users.noreply.github.com>
2023-07-08 13:04:28 +0200

    f
    f

--- src/components/landingpagesections.jsx
+++ src/components/landingpagesections.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 import Link from "next/link";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
 export default function LandingPageSections() {
+  const breakpoints = {
+    // when window width is >= 640px (sm)
+    640: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px (md)
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 1024px (lg)
+    1024: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1280px (xl)
+    1280: {
+      slidesPerView: 4,
+    },
+  };
+
   return (
     <>
       <section className="bg-white dark:bg-gray-900 my-10">
@@ -217,169 +242,6 @@ export default function LandingPageSections() {
         </div>
       </section>
 
-      {/* <div className="font-bold">
-        {" "}
-        <h1 className="font-bold text-white text-center py-5 text-xl">
-          Why people choose Goopim
-        </h1>
-      </div>
-      <div className="flex flex-col items-center sm:flex-row sm:justify-around m-4 text-white py-10">
-        <div className="sm:w-1/3 pb-4">
-          <h1 className="text-primary font-bold">Save Time</h1>
-          <p className="font-300">
-            Goopim helps you increase your earnings by winning more jobs and
-            getting paid on time with professional payment requests. With
-            Goopim, you can grow your freelance business and increase your
-            earnings over time.
-          </p>
-        </div>
-
-        <div className="sm:w-1/3 rounded-2xl">
-          <img src="/savetime.jpg" className="rounded-full " />
-        </div>
-      </div> */}
-
-      {/* <div className="flex flex-col items-center sm:flex-row sm:justify-around m-4 text-white py-10">
-        <div className="hidden sm:block w-1/3">
-          <img src="/increase-earning.webp" className="rounded-full" />
-        </div>
-        <div className="sm:w-1/3 pb-4">
-          <h1 className="text-primary font-bold">Increases Earnings</h1>
-          <p className="font-300">
-            Goopim helps you increase your earnings by winning more jobs and
-            getting paid on time with professional payment requests. With
-            Goopim, you can grow your freelance business and increase your
-            earnings over time.
-          </p>
-        </div>
-
-        <div className="sm:hidden">
-          <img src="/increase-earning.webp" className="rounded-full" />
-        </div>
-      </div> */}
-
-      {/* <div className="flex flex-col items-center sm:flex-row sm:justify-around m-4 text-white py-10">
-        <div className="sm:w-1/3 pb-4">
-          <h1 className="text-primary font-bold">
-            Connect to global workforce
-          </h1>
-          <p className="font-300">
-            Goopim helps you increase your earnings by winning more jobs and
-            getting paid on time with professional payment requests. With
-            Goopim, you can grow your freelance business and increase your
-            earnings over time.
-          </p>
-        </div>
-
-        <div className="sm:w-1/3">
-          <img src="/customersupport.png" className="rounded-full" />
-        </div>
-      </div> */}
-
-      {/* <div className="sm:py-10">
-        <div className="pl-4 flex justify-around mb-5">
-          <div className="bg-white  border-2 border-grey-700 p-2 w-1/3">
-            <div className="flex ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-
-              <h2 className="font-bold ml-2">Stick to your budget</h2>
-            </div>
-            <p>
-              Find the right service for every price point. No hourly rates,
-              just project-based pricing.
-            </p>
-          </div>
-          <div className="bg-white  border-2 border-grey-700 p-2  w-1/3">
-            <div className="flex ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
-                />
-              </svg>
-
-              <h2 className="font-bold ml-2">Get quality work done quickly</h2>
-            </div>
-            <p>
-              Hand your project over to a talented freelancer in minutes, get
-              long-lasting results.
-            </p>
-          </div>
-        </div>
-        <div className="pl-4 flex justify-around mt-4  ">
-          <div className="bg-white  border-2 border-grey-700 p-2 w-1/3">
-            <div className="flex ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-                />
-              </svg>
-
-              <h1 className="font-bold ml-2">Pay when you're happy</h1>
-            </div>
-            <p>
-              {" "}
-              Upfront quotes mean no surprises. Payments only get released when
-              you approve.
-            </p>
-          </div>
-          <div className="bg-white  border-2 border-grey-700 p-2  w-1/3">
-            <div className="flex ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"
-                />
-              </svg>
-
-              <h2 className="font-bold ml-2">Count on 24/7 support</h2>
-            </div>
-            <p>
-              Our round-the-clock support team is available to help anytime,
-              anywhere.
-            </p>
-          </div>
-        </div>
-      </div> */}
-
       {/* our service section */}
       <section id="services" className="py-16">
         <div className="container mx-auto">
@@ -524,114 +386,67 @@ export default function LandingPageSections() {
         </div>
       </section>
 
-      {/* feature section */}
-      <div id="feature" className="bg-blue-100 py-12">
-        <div className="container mx-auto">
-          <div className="flex flex-wrap items-center">
-            <div className="w-full lg:w-1/2">
-              <div className="mb-5 lg:mb-0">
-                <h2
-                  className="mb-12 section-heading wow fadeInDown"
-                  data-wow-delay="0.3s"
-                >
-                  Learn More About Us
-                </h2>
-
-                <div className="flex flex-wrap">
-                  <div className="w-full sm:w-1/2 lg:w-1/2">
-                    <div className="m-3">
-                      <div className="icon text-4xl">
-                        <i className="lni lni-layers"></i>
-                      </div>
-                      <div className="features-content">
-                        <h4 className="feature-title">Built with Next JS</h4>
-                        <p>
-                          {" "}
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Veniam tempora quidem vel sint.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="w-full sm:w-1/2 lg:w-1/2">
-                    <div className="m-3">
-                      <div className="icon text-4xl">
-                        <i className="lni lni-gift"></i>
-                      </div>
-                      <div className="features-content">
-                        <h4 className="feature-title">Free to Use</h4>
-                        <p>
-                          {" "}
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Veniam tempora quidem vel sint.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="w-full sm:w-1/2 lg:w-1/2">
-                    <div className="m-3">
-                      <div className="icon text-4xl">
-                        <i className="lni lni-laptop-phone"></i>
-                      </div>
-                      <div className="features-content">
-                        <h4 className="feature-title">Fully Responsive</h4>
-                        <p>
-                          {" "}
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Veniam tempora quidem vel sint.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="w-full sm:w-1/2 lg:w-1/2">
-                    <div className="m-3">
-                      <div className="icon text-4xl">
-                        <i className="lni lni-leaf"></i>
-                      </div>
-                      <div className="features-content">
-                        <h4 className="feature-title">Easy to Customize</h4>
-                        <p>
-                          {" "}
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Veniam tempora quidem vel sint.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full lg:w-1/2">
-              <div
-                className="mx-3 lg:mr-0 lg:ml-3 wow fadeInRight"
-                data-wow-delay="0.3s"
+      {/* cta */}
+      <section className="bg-white dark:bg-gray-900">
+        <div className="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="place-self-center mr-auto lg:col-span-7">
+            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">
+              Find the right freelancer using AI
+            </h1>
+            <p className="mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde
+              perspiciatis dicta labore nulla beatae quaerat quia incidunt
+              laborum aspernatur...
+            </p>
+            <a
+              href="#"
+              className="inline-flex justify-center items-center py-3 px-5 mr-3 text-base font-medium text-center text-white rounded-lg bg-[#42b5d3] hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            >
+              Get started
+              <svg
+                className="ml-2 -mr-1 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <img src="/feature/img-1.svg" alt="" />
-              </div>
-            </div>
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+            <a
+              href="#"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            >
+              Join for free
+            </a>
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="/feature/img-1.svg" alt="mockup" />
           </div>
         </div>
-      </div>
+      </section>
 
       {/* showcase section */}
-      <div className="container mx-auto">
-        <div className="grid mx-auto gap-x-6 gap-y-12 md:grid-cols-2 lg:grid-cols-3 xl:gap-x-8 2xl:gap-x-12 2xl:gap-y-16 xl:gap-y-14 py-12 px-3">
+      <div className="container mx-auto pt-12 pb-24">
+        {/* <div className="grid mx-auto gap-x-6 gap-y-12 md:grid-cols-2 lg:grid-cols-3 xl:gap-x-8 2xl:gap-x-12 2xl:gap-y-16 xl:gap-y-14 py-12 px-3"> */}
+        <Swiper
+          spaceBetween={20}
+          breakpoints={breakpoints}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
           {[1, 2, 3, 4, 5, 6].map((i, index) => (
-            <div>
-              <div className="flex flex-col w-full overflow-hidden bg-gray-100 rounded-2xl h-72 sm:h-80 md:h-72 lg:h-64 xl:h-80">
+            <SwiperSlide>
+              <div className="flex flex-col w-full overflow-hidden bg-gray-100 h-[450px]">
                 <div className="relative flex items-center justify-center flex-shrink-0 h-full group">
                   <img
-                    className="w-9/10 sm:w-10/12 lg:w-9/10 xl:w-10/12 h-auto rounded-lg shadow-md mx-auto object-cover object-left-top transition ease-in-out duration-300"
+                    className="w-full h-full rounded-lg shadow-md mx-auto object-cover object-left-top transition ease-in-out duration-300"
                     src="/increase-earning.webp"
                   />
-                  <div className="absolute inset-0 transition duration-200 bg-gray-900 opacity-0 rounded-2xl group-hover:opacity-60"></div>
-                  <div className="absolute inset-0 flex flex-col items-center justify-center transition duration-200 opacity-0 group-hover:opacity-100">
-                    <div className="shadow-sm w-33 rounded-2xl">
-                      <a className="w-full justify-center inline-flex items-center px-6 py-2 border border-transparent text-sm font-medium rounded-2xl shadow-sm text-white transition duration-150 bg-[#42b5d3] hover:bg-[#42b5d3] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cool-indigo-500 cursor-pointer">
-                        View details
-                      </a>
-                    </div>
-                  </div>
                 </div>
               </div>
               <div>
@@ -654,9 +469,10 @@ export default function LandingPageSections() {
                   </div>
                 </div>
               </div>
-            </div>
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
+        {/* </div> */}
       </div>
     </>
   );
