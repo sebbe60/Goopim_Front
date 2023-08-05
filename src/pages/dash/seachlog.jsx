a058acd0d6f44dfd3faee8301c98fb39149a05d0
John Ugbor <ugborjohn@gmail.com>
2023-08-05 11:11:06 +0100

    searchlog dashboard view

--- src/pages/dash/seachlog.jsx
+++ src/pages/dash/seachlog.jsx
@@ -27,15 +27,42 @@ const SearchLogsComponent = () => {
   const handlePrevPage = () => {
     setCurrentPage(currentPage - 1);
   };
+  function formatDate(dateString) {
+    const dateObject = new Date(dateString);
 
+    const year = dateObject.getFullYear();
+    const month = dateObject.getMonth() + 1;
+    const day = dateObject.getDate();
+
+    return `${year}-${month}-${day}`;
+  }
   return (
     <div>
-      <h2>Search Logs</h2>
-      {searchLogs.map((searchlog) => (
-        <div key={searchlog.id}>
-          <p>ID: {searchlog.id}</p>
-          <p>Text: {searchlog.text}</p>
-          <p>Creation Date: {searchlog.creation_date}</p>
+      <h2 className="p-2">Search Logs</h2>
+      <div className="flex w-full justify-between bg-[#5AB6D4] py-2">
+        <div className="w-10 ">
+          {" "}
+          <p className="pl-2">ID</p>
+        </div>
+        <div className="w-75 ">
+          {" "}
+          <p>Text</p>
+        </div>
+        <div className="w-15 ">
+          {" "}
+          <p className="pr-2">Creation Date</p>
+        </div>
+      </div>
+      {searchLogs.map((searchlog, index) => (
+        <div
+          key={searchlog.id}
+          className={`flex justify-between ${
+            index % 2 === 0 ? "border-b" : "border-t"
+          }`}
+        >
+          <p className="pl-2">{searchlog.id}</p>
+          <p className="pl-2">{searchlog.text}</p>
+          <p className="pr-2">{formatDate(searchlog.creation_date)}</p>
         </div>
       ))}
       <div>
