db1f768f043419f8a76dd84bbd083a4a88b7bca5
sebbe60 <130213748+sebbe60@users.noreply.github.com>
2023-08-17 15:45:04 +0200

    Swedish translation to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -19,12 +19,10 @@ export default function HomePage() {
         <div className="mx-auto max-w-2xl py-32 sm:py-35 lg:py-45">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Find the best freelancer using AI
+              Vilken talang behöver ditt projekt?
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              Goopim har en enorm databas med talang och använder AI för att du ska enkelt kunna hitta den bästa talangen för ditt projekt
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
@@ -34,7 +32,7 @@ export default function HomePage() {
                 focus-visible:outline focus-visible:outline-2
                 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                Börja nu
               </Link>
             </div>
           </div>
