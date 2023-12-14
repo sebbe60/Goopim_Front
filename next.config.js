3a0443158f04d1d8a4288fb10cbb2180bd981c2a
sebbe60 <130213748+sebbe60@users.noreply.github.com>
2023-12-14 14:55:13 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,49 @@
 const nextConfig = {
   images: {
     disableStaticImages: true,
+  },
+  webpack: (config, options) => {
+    const { isServer } = options;
+    config.module.rules.push({
+      test: /\.(wav|mp3|ogg|mpe?g|png|jpe?g|gif|svg)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: require.resolve("file-loader"),
+          options: {
+            limit: 10000,
+            fallback: require.resolve("url-loader"),
+            publicPath: `_next/static/images/`,
+            outputPath: `${isServer ? "../" : ""}static/images/`,
+            name: "[name]-[hash].[ext]",
+            esModule: config.esModule || false,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+  reactStrictMode: false,
+};
+
+module.exports = nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const nextConfig = {
+  images: {
+    disableStaticImages: true,
     unoptimized: true,
   },
   webpack: (config, options) => {
@@ -25,7 +68,7 @@ const nextConfig = {
     });
     return config;
   },
-  reactStrictMode: true,
+  reactStrictMode: true, */
   distDir: 'build',
   // output: 'export',
 };
